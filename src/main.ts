e38afdb3f2c9e96bd3b4f968c0a624d41310d6ac
Dang Huu Duong <danghuuduong98@gmail.com>
2025-04-16 00:24:27 +0700

    tesst2

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: ['https://sunbtc.vercel.app', 'http://localhost:5174'],
-    methods: ['GET', 'POST', 'PUT'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Cho phép gửi cookies và các dữ liệu xác thực khác (nếu có)
-  };
-  app.enableCors(corsOptions);
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
