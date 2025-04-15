e82348fce4386966eea74258fe18e93b22224b82
Dang Huu Duong <danghuuduong98@gmail.com>
2025-04-16 00:56:18 +0700

    update apmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CandleModule } from './candle/candle.module';
+import { StatusTradingModule } from './start-trading/start-trading.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { MyInfomationModule } from './my-infomation-from-binance/my-infomation.module';
+import { realtimeBTCWebsoketModule } from './realtimeBTC-websoket/realtimeBTC-websoket.module';
+import { OrderHistoryModule } from './orderHistory/orderHistory.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
 
 @Module({
-  imports: [],
+  imports: [
+    CandleModule,
+    realtimeBTCWebsoketModule,
+    StatusTradingModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    MyInfomationModule,
+    OrderHistoryModule,
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
